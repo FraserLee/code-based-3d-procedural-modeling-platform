5cbcd56ec17fb20de09e4f6a562767d2f58098b8
Unknown <tomatoesneverlie@gmail.com>
2021-06-13 02:25:24 -0400

    Added (super basic) shader generation through code

--- app/core/shader-elements.yaml.glsl
+++ app/core/shader-elements.yaml.glsl
@@ -2,12 +2,12 @@
 # head, tail might be a useful paradigm sometimes, but suspect dependency-reference will be far more commom in practice
 
 minimal:
-  head: |
+  head: |-
     #version 300 es
     precision mediump float;
     out vec4 fragColor;
     void main() {
       fragColor = vec4(
-  tail: |
-    ,1.0);
+  tail: |-
+    , 1.0);
     }
--- app/core/src/lib.rs
+++ app/core/src/lib.rs
@@ -1,7 +1,8 @@
+#![feature(format_args_capture)]
 use neon::prelude::*;
 use yaml_rust::{YamlLoader, yaml::Yaml};
 use lazy_static::lazy_static;
-
+use rand::{Rng, thread_rng, seq::SliceRandom};
 
 lazy_static! {
 	/// A hashmap-style way to access various pieces of shader-code
@@ -31,14 +32,26 @@ fn load_vert(mut cx: FunctionContext) -> JsResult<JsString> {
 /// As of the present moment, next to none of this is implemented
 
 fn build_shader(mut cx: FunctionContext) -> JsResult<JsString> {
-	Ok(cx.string(include_str!("minimal.frag")))
+	let mut rand = thread_rng();
+	// One of the RGB values will always be 1.0, giving us colours colours with a value of 1.
+	// Fixing one of the other two to 0.25 keeps saturation at 62%, with the third (random) value
+	// controlling hue.
+	let mut colour = vec![rand.gen::<f32>(), 0.25, 1.0];
+	colour.shuffle(&mut rand);
+	if let [r, g, b] = &colour[..]{
+		// amends and returns the head and tail of the minimal shader, with our random colour
+		// inserted in the middle.
+		return Ok(cx.string(
+				format!("{head}{r:.3}, {g:.3}, {b:.3}{tail}", 
+					head=&SHADER_ELEMENTS["minimal"]["head"].as_str().unwrap(), 
+					tail=&SHADER_ELEMENTS["minimal"]["tail"].as_str().unwrap())
+		));
+	}
+	Ok(cx.string(""))
 }
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-	println!("{:?}", &SHADER_ELEMENTS["minimal"]["head"].as_str().unwrap());
-	println!("{:?}", &SHADER_ELEMENTS["minimal"]["tail"].as_str().unwrap());
-
 	cx.export_function("load_vert", load_vert)?;
 	cx.export_function("build_shader", build_shader)?;
 	Ok(())
